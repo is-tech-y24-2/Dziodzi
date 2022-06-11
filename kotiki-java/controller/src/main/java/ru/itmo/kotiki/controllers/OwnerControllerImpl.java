ee69c760faa2a98b1e95ba2f1e28f993feafa2b8
Gregory <gsh812@inbox.ru>
2022-06-11 15:59:50 +0300

    extra commit for correct CI

--- kotiki-java/controller/src/main/java/ru/itmo/kotiki/controllers/OwnerControllerImpl.java
+++ kotiki-java/controller/src/main/java/ru/itmo/kotiki/controllers/OwnerControllerImpl.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
+
 import ru.itmo.kotiki.entity.Owner;
 import ru.itmo.kotiki.entity.Ownership;
 import ru.itmo.kotiki.entity.ServiceOwnerImpl;
